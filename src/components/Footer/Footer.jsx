e7bd4262742c4071306183fd644a2d2445f997a4
Zibusiso Ndlovu <zeendlovu15@gmail.com>
2024-02-29 22:11:28 +0200

    Footer complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,47 @@
 import './footer.scss'
+import { faTwitter, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footerContainer">
                 <div className='left-mid'>
-                    <h1 className='title'>Left Mid Container</h1>
-                    <p className=''></p>
+                    <h2 className='title'>Standard Telephones and Cables</h2>
+                    <p className=''>STC is a technology company dedicated to applying technology to reduce costs, improve 
+                    efficiencies and contribute towards value adding for its clients.</p>
                 </div>
                 <div className='mid'>
-                    <h1>Mid Container</h1>
+                    <h2>Our Services</h2>
+                    <ul>
+                        <li>Amazon Web Services (AWS)</li>
+                        <li>3CX Unified Communications</li>
+                        <li>Freshwork CRM</li>
+                        <li>CCTV and Access Control</li>
+                        <li>Video Analytics (AI)</li>
+                    </ul>
                 </div>
                 <div className='right'>
-                    <h1>Right Container</h1>
+                <h2>Office in Harare</h2>
+                    <ul>
+                        <li>714 Steven Drive Msasa, Harare</li>
+                        <li>(+263) 8688-002-260</li>
+                        <li>(+263) 242-486-781-5</li>
+                        <li><a href="mailto:sales@stc.co.zw">sales@stc.co.zw</a></li>
+                        <li><a href="mailto:sales@stc.co.zw">marketing@stc.co.zw</a></li>
+                    </ul>
                 </div>
             </div>
             <div className='bottomFooter'>
                 <div className='copyright'>
-                    <p>Standard Telephones and Cables | 2024</p>
+                    <p>Copyright 2024, Standard Telephones and Cables. All rights reserved.</p>
                 </div>
                 
                 <div className='icons'>
-                    <h3>A</h3>
-                    <h3>B</h3>
-                    <h3>C</h3>
+                    <h3><FontAwesomeIcon icon={faTwitter} /></h3>
+                    <h3><FontAwesomeIcon icon={faFacebook} /></h3>
+                    <h3><FontAwesomeIcon icon={faLinkedin} /></h3>
                 </div>
             </div>           
         </div>
