9dc37655f47193e0ceec6f6f9bd9194e69771aba
Zibusiso Ndlovu <zeendlovu15@gmail.com>
2024-03-06 13:04:02 +0200

    modal in progress

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,8 +8,61 @@ import phoneSystem from '../../static/images/3CX-Logo.png'
 import aws from '../../static/images/aws1.png'
 import freshworks from '../../static/images/freshworks-800x450.png'
 
+import { useState } from 'react'
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '80%',
+  height: '80%',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
+
+const KeepMountedModal = ({ isOpen, onClose, title, content, header1, header2, header3 }) => {
+  return (
+    <Modal
+      keepMounted
+      open={isOpen}
+      onClose={onClose}
+      aria-labelledby="keep-mounted-modal-title"
+      aria-describedby="keep-mounted-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
+          {title}
+        </Typography>
+        <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
+          <h3>{header1}</h3>
+          {content}
+        </Typography>
+      </Box>
+    </Modal>
+  );
+};
 
 const Home = () => {
+  const [modal1Open, setModal1Open] = useState(false);
+  const [modal2Open, setModal2Open] = useState(false);
+  const [modal3Open, setModal3Open] = useState(false);
+
+  const handleModal1Open = () => setModal1Open(true);
+  const handleModal1Close = () => setModal1Open(false);
+
+  const handleModal2Open = () => setModal2Open(true);
+  const handleModal2Close = () => setModal2Open(false);
+
+  const handleModal3Open = () => setModal3Open(true);
+  const handleModal3Close = () => setModal3Open(false);
+
   return(
       <div className='home'>
         <div className='homeContainer'>
@@ -34,22 +87,44 @@ const Home = () => {
           <div className='services'>
             <div className='service'>
               <img src={cloud_image} alt='cloud' width='100%' height='70%' />
-              <h3>Cloud Services</h3>
+              <Button onClick={handleModal1Open}>Cloud Services</Button>
+              <KeepMountedModal
+              isOpen={modal1Open}
+              onClose={handleModal1Close}
+              title="Cloud Services"
+              header1= "Migration"
+              content="Content for Modal 1 goes here."
+              />  
             </div>
+
+
             <div className='service'>
             <img src={unified} alt='cloud' width='100%' height='70%' />
-            <h3>Unified Communications</h3>
+            <Button onClick={handleModal2Open}>Communications</Button>
+            <KeepMountedModal
+            isOpen={modal2Open}
+            onClose={handleModal2Close}
+            title="Communications"
+            content="Content for Modal 2 goes here."/>
             </div>
+
             <div className='service'>
             <img src={security} alt='cloud' width='100%' height='70%' />
-            <h3>Security Services</h3>
+            <Button onClick={handleModal3Open}>Security Services</Button>
+          <KeepMountedModal
+            isOpen={modal3Open}
+            onClose={handleModal3Close}
+            title="Security Services"
+            content="Content for Modal 3 goes here."/>
             </div>
           </div>
 
         <div className='content-element'>
           <div className='left'>
           <img src={ cloud } alt='cloud' width='100%' height='100%' />
+          
           </div>
+          
           <div className='right'>
             <h1>Your Cloud, Your Way</h1>
             <p>Whether your business is early in its journey or well on its way to digital transformation, our cloud solutions 
